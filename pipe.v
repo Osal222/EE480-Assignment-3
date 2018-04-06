9f6ba9619e3956044198c38f5f46c7799a0b5af7
Osal222 <35607860+Osal222@users.noreply.github.com>
2018-04-06 14:24:30 -0400

    Update pipe.v

--- pipe.v
+++ pipe.v
@@ -1,232 +1,244 @@
-//These need to be checked and corrected
-`define WORD          [15:0]
-`define OPCode		[15:12]
-`define Dest		[11:8]
-`define Src           [7:4]
-`define Alt           [3:0]
-`define Imm           [7:0]
-`define EN            [31:0]
-`define STATE         [3:0]
-`define OP            [4:0]
+// Basic
+`define WORD			[15:0]
+`define OpCode			[15:12]
+`define Dest			[11:8]
+`define Sc				[7:4]
+`define alt				[3:0]
+`define Imm				[7:0]
+`define EN				[31:0]
+`define STATE			[3:0]
+`define Op				[4:0]
 
-// This stuff should be fine
-`define REGSIZE        [15:0]
-`define MEMSIZE        [65535:0]
-`define RETADDR        [63:0]
-`define REGNAME        [3:0]
-`define ESTACKSIZE     [31:0]
-`define CSTACKSIZE     [63:0]
+// Size references
+`define REGSIZE			[15:0]
+`define MEMSIZE			[65535:0]
+`define RETADDR			[63:0]
+`define REGNAME			[3:0]
+`define ESTACKSIZE		[31:0]
+`define CSTACKSIZE		[63:0]
 
-// 4bit opcodes 
-`define OPAdd 4'b0000
-`define OPAnd 4'b0001
-`define OPMul 4'b0010
-`define OPOr 4'b0011
-`define OPSll		4'b0100
-`define OPSlt		4'b0101
-`define OPSra		4'b0110
-`define OPXor		4'b0111
-`define OPGor         4'b1000
-`define OPLeft        4'b1001
-`define OPRight       4'b1010
-`define OPLnot        4'b1011
-`define OPNeg         4'b1100
-`define OPLI8		4'b1101
-`define OPLU8		4'b1110
-`define OP8Reg		4'b1111
+// 4bit Operatrors
+`define OpAdd		4'b0000
+`define OpAnd		4'b0001
+`define OpMul		4'b0010
+`define OpOr		4'b0011
+`define OpSll		4'b0100
+`define OpSlt		4'b0101
+`define OpSra		4'b0110
+`define OpXor		4'b0111
+`define OpGor		4'b1000
+`define OpLeft		4'b1001
+`define OpRight		4'b1010
+`define OpLnot		4'b1011
+`define OpNeg		4'b1100
+`define OpLI8		4'b1101
+`define OpLU8		4'b1110
+`define OpSc		4'b1111
 
-//extended code
-`define OPLoad 5'b10000
-`define OPStore 5'b10001
-`define OPAllen		5'b10010
-`define OPPopen		5'b10011
-`define OPPushen 	5'b10100
-`define OPRet		5'b10101
-`define OPNop		5'b10110
-`define OPTrap		5'b10111
-`define OPCall		5'b11000
-`define OPJump		5'b11001
-`define OPJumpf		5'b11010
-
-
-//SRC values
-`define SRCLoad       4'b0000
-`define SRCStore      4'b0001
-`define SRCAllen      4'b0010
-`define SRCPopen      4'b0011
-`define SRCPushen     4'b0100
-`define SRCRet        4'b0101
-`define SRCNop		4'b0110
-`define SRCTrap		4'b0111
-`define SRCCall		4'b1000
-`define SRCJump		4'b1001
-`define SRCJumpf	4'b1010
+// 5bit extended Operatrors  
+`define OpLoad 		5'b10000
+`define OpStore		5'b10001
+`define OpAllen		5'b10010
+`define OpPOpen		5'b10011
+`define OpPushen 	5'b10100
+`define OpRet		5'b10101
+`define OpNOp		5'b10110
+`define OpTrap		5'b10111
+`define OpCall		5'b11000
+`define OpJump		5'b11001
+`define OpJumpf		5'b11010
 
 
+//Source Codes
+`define ScLoad		4'b0000
+`define ScStore		4'b0001
+`define ScAllen		4'b0010
+`define ScPOpen		4'b0011
+`define ScPushen	4'b0100
+`define ScRet		4'b0101
+`define ScNOp		4'b0110
+`define ScTrap		4'b0111
+`define ScCall		4'b1000
+`define ScJump		4'b1001
+`define ScJumpf		4'b1010
 
 
 /////////////////////////////////////////////////////////////////////
-module decode(opout, regd, in, ir);
-output reg `OP opout;
+///////////////module decode/////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+module decode(Opout, regd, in, ir);
+output reg `Op Opout;
 output reg `REGNAME regd;
 
-input wire `OP in;
+input wire `Op in;
 input `WORD ir;
 
 always @(in, ir) begin
-	if((in == `OPJumpf) || (in == `OPJump) || (in == `OPCall)) begin
-		opout = `OPNop;  // 2nd word of li becomes nop
+	if((in == `OpJumpf) || (in == `OpJump) || (in == `OpCall)) begin
+		Opout = `OpNOp;  // 2nd word of li becomes nOp
 		regd = 0;           // No writing will occur
 	end else begin
-		case (ir `OPCode)
-			`OP8Reg:begin
+		case (ir `OpCode)
+			`OpSc:begin
 			regd=0;
 			case (ir `Dest)
-				`SRCLoad: opout = `OPLoad;
-				`SRCStore: opout = `OPStore;
-				`SRCAllen: opout = `OPAllen;
-				`SRCPopen: opout = `OPPopen;
-				`SRCPushen: opout = `OPPushen;
-				`SRCRet: opout = `OPRet;
-				`SRCNop: opout = `OPNop;
-				`SRCTrap: opout = `OPTrap;
-				`SRCCall: opout = `OPCall;
-				`SRCJump: opout = `OPJump;
-				`SRCJumpf: opout = `OPJumpf;
-    			endcase
+				`ScLoad:		Opout = `OpLoad;
+				`ScStore:		Opout = `OpStore;
+				`ScAllen:		Opout = `OpAllen;
+				`ScPOpen:		Opout = `OpPOpen;
+				`ScPushen:		Opout = `OpPushen;
+				`ScRet:			Opout = `OpRet;
+				`ScNOp:			Opout = `OpNOp;
+				`ScTrap:		Opout = `OpTrap;
+				`ScCall:		Opout = `OpCall;
+				`ScJump:		Opout = `OpJump;
+				`ScJumpf:		Opout = `OpJumpf;
+    		endcase
       	end
-      	`OPStore: begin opout = ir `OPCode; regd <= 0; end
-           default: begin opout = ir `OPCode; regd <= ir `Dest; end
+      	`OpStore: begin Opout = ir `OpCode; regd <= 0; end
+        default: begin Opout = ir `OpCode; regd <= ir `Dest; end
     endcase
-  end
+end
 end
 endmodule
-
-
 /////////////////////////////////////////////////////////////////////
 
 
-
-// ALU is finished for the most part
+////////////////////module alu///////////////////////////////////////
 /////////////////////////////////////////////////////////////////////
-module alu(result, OPCode, Instruct1, Instruct2);
+module alu(result, OpCode, Inst1, Inst2);
 output reg `WORD result;
-input wire `OP OPCode;
-input wire `WORD Instruct1, Instruct2;
+input wire `Op OpCode;
+input wire `WORD Inst1, Inst2;
 
-always @(OPCode, Instruct1, Instruct2) begin
-  case (OPCode)
-	`OPAdd:   begin result = Instruct1 + Instruct2; end
-	`OPAnd:   begin result = Instruct1 & Instruct2; end
-	`OPMul:   begin result = Instruct1 * Instruct2; end
-	`OPOr:    begin result = Instruct1 | Instruct2; end
-	`OPGor:   begin result = Instruct1; end 
-	`OPXor:   begin result = Instruct1 ^ Instruct2; end
-	`OPSll:   begin result = Instruct1 << (Instruct2 & 4'b1111); end 
-	`OPSra:   begin result = Instruct1 >> (Instruct2 & 4'b1111); end
-	`OPLnot:  begin result = !Instruct1; end
-	`OPNeg:   begin result = ~Instruct1; end
-	`OPSlt:   begin result = (Instruct1 < Instruct2)? 4'b1 : 4'b0;end
-	`OPLeft:  begin result = Instruct1; end
-	`OPRight: begin result = Instruct1; end
-	default:  begin result = Instruct1; end
+always @(OpCode, Inst1, Inst2) begin
+  case (OpCode)
+	`OpAdd:		result = Inst1 + Inst2;
+	`OpAnd:		result = Inst1 & Inst2;
+	`OpMul:		result = Inst1 * Inst2;
+	`OpOr:		result = Inst1 | Inst2;
+	`OpGor:		result = Inst2; 
+	`OpXor:		result = Inst1 ^ Inst2;
+	`OpSll:		result = Inst1 << (Inst2 & 4'b1111); 
+	`OpSra:		result = Inst1 >> (Inst2 & 4'b1111);
+	`OpLnot:	result = !Inst2;
+	`OpNeg:		result = ~Inst2;
+	`OpSlt:		result = (Inst1 < Inst2)? 4'b0001 : 4'b0000;
+	`OpLeft:	result = Inst2;
+	`OpRight:	result = Inst2;
+	default:	result = Inst1;
   endcase
 end
 endmodule
 /////////////////////////////////////////////////////////////////////
 
-// Still working on processor
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////module processor////////////////////////////
 /////////////////////////////////////////////////////////////////////
 module processor(halt, reset, clk);
 output reg halt;
 input reset, clk;
 
 reg  `WORD RegFile `REGSIZE;
-reg  `WORD InstructMem `MEMSIZE;
+reg  `WORD InstMem `MEMSIZE;
 reg  `WORD DataMem `MEMSIZE;
-reg  `WORD NewPC, IReg, SrcVal, DstVal;
-wire `OP DecOP;
+reg  `WORD NewPC, IReg, ScVal, altVal, DstVal;
+wire `Op DecOp;
 wire `REGNAME RegDst;
 wire `WORD ALUResult;
 reg  `ESTACKSIZE en = 1;
 reg  `CSTACKSIZE CallStack,Callstacktemp;
-reg  `OP s0OP, s1OP, s2OP;
-reg  `REGNAME s0Src, s0Dst, s0RegDst, s1RegDst, s2RegDst;
+reg  `Op s0Op, s1Op, s2Op;
+reg  `REGNAME s0Sc, s0alt, s0Dst, s0RegDst, s1RegDst, s2RegDst;
 reg  `WORD PC;
-reg  `WORD s1SrcVal, s1DstVal;
+reg  `WORD s1ScVal, s1altVal;
 reg  `WORD s2Val;
 
 always @(reset) begin
     halt = 0;
     PC = 0;
-    s0OP = `OPNop;
-    s1OP = `OPNop;
-    s2OP = `OPNop;
+    RegFile[0] = 0; // zero
+    RegFile[1] = 0; // IPROC
+    RegFile[2] = 1; // NPROC
+    en = -1; // all 1s
+    s0Op = `OpNOp;
+    s1Op = `OpNOp;
+    s2Op = `OpNOp;
     $readmemh0(RegFile);
-    $readmemh1(InstructMem);
+    $readmemh1(InstMem);
     $readmemh2(DataMem);
 end
 
-decode MyDecode(DecOP, RegDst, s0OP, IReg);
-alu ALU(ALUResult, s1OP, s1SrcVal, s1DstVal);
+decode MyDecode(DecOp, RegDst, s0Op, IReg);
+alu ALU(ALUResult, s1Op, s1ScVal, s1altVal);
 
-always @(*) IReg = InstructMem[PC];
+always @(*) IReg = InstMem[PC];
 
-// new pc value
+// new pc value. Can come from many places
 always @(*) begin 
-    NewPC = ((s1OP == `OPJumpf) && (s1DstVal == 0)) ? s1SrcVal :
-        (s1OP == `OPJump) ? s1SrcVal: ((s1OP == `OPCall) &&     
-        (en[0] == 1)) ? s1SrcVal: ((s1OP == `OPRet) &&(en[0] == 1)) ?          
+    NewPC = ((s1Op == `OpJumpf) && (s1altVal == 0)) ? s1ScVal :
+        (s1Op == `OpJump) ? s1ScVal: ((s1Op == `OpCall) &&     
+        (en[0] == 1)) ? s1ScVal: ((s1Op == `OpRet) &&(en[0] == 1)) ?          
         CallStack[15:0] : (PC + 1); 
 		Callstacktemp = CallStack;
-    if(s1OP == `OPJumpf && s1DstVal == 0) begin
+    if(s1Op == `OpJumpf && s1altVal == 0) begin
         en[0] = ~en[0];
     end
-    if(s1OP == `OPCall && en[0] == 1) begin
+    if(s1Op == `OpCall && en[0] == 1) begin
 		
         CallStack = {Callstacktemp[47:0], PC};
     end
-    if(s1OP == `OPRet && en[0] == 1) begin
+    if(s1Op == `OpRet && en[0] == 1) begin
         CallStack = {Callstacktemp[63:48], Callstacktemp[63:16]};
     end
 end
 
-// compute RR SrcVal, with value forwarding... also from 2nd word of li
-always @(*) begin 
-    if ((s0OP == `OPJump) || (s0OP == `OPJumpf) || (s0OP == `OPCall))     
-        SrcVal = IReg; // catch immediate for li
-    else SrcVal = ((s1RegDst && (s0Src == s1RegDst)) ? ALUResult :
-        ((s2RegDst && (s0Src == s2RegDst)) ? s2Val :RegFile[s0Src]));
-end
+// compute RR ScVal, with value forwarding...
+always @(*)
+	if ((s0Op == `OpJump) || (s0Op == `OpJumpf) || (s0Op == `OpCall) || (s0Op == `OpLI8) || (s0Op == `OpLU8))      
+        ScVal = IReg;
+    else ScVal = ((s1RegDst && (s0Sc == s1RegDst)) ? ALUResult :
+					((s2RegDst && (s0Sc == s2RegDst)) ? s2Val :
+						RegFile[s0Sc]));
 
-// compute RR DstVal, with value forwarding
+// compute DstVal, with value forwarding
 always @(*) DstVal = ((s1RegDst && (s0Dst == s1RegDst)) ? ALUResult :
-    ((s2RegDst && (s0Dst == s2RegDst)) ? s2Val : RegFile[s0Dst]));
+                      ((s2RegDst && (s0Dst == s2RegDst)) ? s2Val :
+                       RegFile[s0Dst]));
 
-// Instruction Fetch
+// compute altVal, with value forwarding...
+always @(*)
+    if ((s0Op == `OpJump) || (s0Op == `OpJumpf) || (s0Op == `OpCall) || (s0Op == `OpLI8) || (s0Op == `OpLU8)) 
+        altVal = 0;
+    else  altVal = (s1RegDst && (s0alt == s1RegDst)) ? ALUResult :
+        ((s0alt == s2RegDst)) ? s2Val :RegFile[s0alt];
+
+
+
+// Inst Fetch
 always @(posedge clk) if (!halt) begin
-    s0OP <= (DecOP == `OPTrap) ? `OPNop : DecOP;
-    s0RegDst <= (DecOP == `OPTrap) ? 0 : RegDst;
-    s0Src <= ir `Src;
-    s0Dst <= ir `Dest;
+    s0Op <= (DecOp == `OpTrap) ? `OpNOp : DecOp;
+    s0RegDst <= (DecOp == `OpTrap) ? 0 : RegDst;
+    s0Sc <= IReg`Sc;
+    s0alt <=IReg `alt;
     PC <= NewPC;
 end
 
 // Register Read
 always @(posedge clk) if (!halt) begin
-    s1OP <= s0OP;
+    s1Op <= s0Op;
     s1RegDst <= s0RegDst;
-    s1SrcVal <= SrcVal;
-    s1DstVal <= DstVal;
+    s1ScVal <= ScVal;
+    s1altVal <= altVal;
 end
 
-// ALU and data memory operations
+// ALU and data memory Operations
 always @(posedge clk) if (!halt) begin
-    s2OP <= s1OP;
-    s2RegDst <= s1regdst;
-    s2Val <= ((s1OP == `OPLoad) ? DataMem[s1SrcVal] : ALUResult);
-    if (s1OP == `OPStore) DataMem[s1SrcVal] <= s1DstVal;
-    //////////if (s1op == `OPsys) halt <= 1;
+    s2Op <= s1Op;
+    s2RegDst <= s1RegDst;
+    s2Val <= ((s1Op == `OpLoad) ? DataMem[s1ScVal] : ALUResult);
+    if (s1Op == `OpStore) DataMem[s1ScVal] <= s1altVal;
 end
 
 // Register Write
@@ -234,3 +246,29 @@ always @(posedge clk) if (!halt) begin
     if (s2RegDst != 0) RegFile[s2RegDst] <= s2Val;
 end
 endmodule
+/////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////
+/////////////////Test Bench//////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+module testbench;
+reg reset = 0;
+reg clk = 0;
+wire halted;
+integer i = 0;
+processor PE(halted, reset, clk);
+initial begin
+  $dumpfile;
+  $dumpvars(0, PE);
+  #10 reset = 1;
+  #10 reset = 0;
+  while (!halted && (i < 50)) begin
+	#10 clk = 1;
+	#10 clk = 0;
+	i=i+1;
+  end
+  $finish;
+end
+endmodule
+/////////////////////////////////////////////////////////////////////
